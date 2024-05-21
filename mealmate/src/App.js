c6d0e00f23854c77f57c866086dec76aebdd243a
SinYusi <yusi4781@naver.com>
2024-05-21 16:18:28 +0900

    데이터 불러오기
    ajax 통신을 통하여 데이터를 불러오고 렌더링 해봤습니다. array 형식으로 받아온다는 것을 잊지 맙시다..

--- mealmate/src/App.js
+++ mealmate/src/App.js
@@ -2,11 +2,42 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Navigationbar from './components/Navigationbar'
 import Login from './Routes/Login'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function dataLoad(setRestaurantData) {
+
+}
 
 function App() {
+  const [restaurantData, setRestaurantData] = useState(0); // 상태 초기화
+  const [isFull, setIsFull] = useState(false);
+  useEffect(() => {
+    axios({
+      method: 'get',
+      url: `https://api.meal-mate.shop/api/restaurant`,
+      params: {
+        type: '분식'  // 쿼리 파라미터 key-value
+      }
+    })
+      .then((response) => {
+        setRestaurantData(response.data)
+        setIsFull(true)
+      })
+      .catch(() => {
+        console.log('실패');
+      })
+  }, [])
+  
+  if (!isFull) {
+    return <div>Loading...</div>; // 데이터가 로드될 때까지 로딩 표시
+  }
+  
   return (
-    <div className="App">
+    <div>
       <Navigationbar />
+      
+      <h2>name: {restaurantData[0].restaurantName}</h2>
       <Routes>
         <Route path='/login' element={<Login />} />
       </Routes>
