ae363e9120932b15fda37c28a31881b351906466
SinYusi <yusi4781@naver.com>
2024-05-24 23:58:20 +0900

    image 파일과 json 객체를 db에 올리기 성공
    image 파일과 object 객체를 json 객체로 변환하여 FormData 형식으로 db에 올리는 것을 성공

--- mealmate/src/Routes/AddRestaurant.js
+++ mealmate/src/Routes/AddRestaurant.js
@@ -8,13 +8,11 @@ function AddRestaurant() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     const formData = new FormData();
-    formData.append('restaurantimage', file);
-    console.log(file)
+    formData.append('imageFile', file);
+    formData.append('restaurantRegister', new Blob([JSON.stringify(restaurantData)], { type: "application/json" }))
     try {
-      await axios.post('https://api.meal-mate.shop/api/restaurant', {
-        imageFile: formData,
-        restaurantRegister: restaurantData
-      })
+      await axios.post('https://api.meal-mate.shop/api/restaurant', formData)
+      alert('성공')
     } catch (error) {
       console.log(error);
       alert('실패')
@@ -22,9 +20,7 @@ function AddRestaurant() {
   }
 
   const onChangeImage = (e) => {
-    console.log('onChange 호출')
     const file = e.target.files[0];
-    console.log(file)
     setFile(file);
   }
 
@@ -36,15 +32,15 @@ function AddRestaurant() {
         <p>식당 카테고리</p>
         <input type="text" onChange={(e) => { setRestaurantData((prevObj) => ({ ...prevObj, restaurantType: e.target.value })) }}></input>
         <p>식당 소개</p>
-        <input type="text" onChange={(e) => { setRestaurantData((prevObj) => ({ ...prevObj, restaurantType: e.target.value })) }}></input>
+        <input type="text" onChange={(e) => { setRestaurantData((prevObj) => ({ ...prevObj, restaurantNotice: e.target.value })) }}></input>
         <p>식당 위치</p>
-        <input type="text" onChange={(e) => { setRestaurantData((prevObj) => ({ ...prevObj, restaurantType: e.target.value })) }}></input>
-        <p>식당 번호</p>
-        <input type="text" onChange={(e) => { setRestaurantData((prevObj) => ({ ...prevObj, restaurantType: e.target.value })) }}></input>
+        <input type="text" onChange={(e) => { setRestaurantData((prevObj) => ({ ...prevObj, location: e.target.value })) }}></input>
         <p>식당 오픈</p>
-        <input type="text" onChange={(e) => { setRestaurantData((prevObj) => ({ ...prevObj, restaurantType: e.target.value })) }}></input>
+        <input type="text" onChange={(e) => { setRestaurantData((prevObj) => ({ ...prevObj, openAt: e.target.value })) }}></input>
         <p>식당 마감</p>
-        <input type="text" onChange={(e) => { setRestaurantData((prevObj) => ({ ...prevObj, restaurantType: e.target.value })) }}></input>
+        <input type="text" onChange={(e) => { setRestaurantData((prevObj) => ({ ...prevObj, closeAt: e.target.value })) }}></input>
+        <p>식당 번호</p>
+        <input type="text" onChange={(e) => { setRestaurantData((prevObj) => ({ ...prevObj, telNum: e.target.value })) }}></input>
         <input type="file" onChange={onChangeImage}></input>
         <button type="submit">추가</button>
       </form>
