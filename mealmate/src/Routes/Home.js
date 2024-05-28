a582beb4641e0844fcd7ec3d656707def6b33fcd
SinYusi <yusi4781@naver.com>
2024-05-28 22:33:16 +0900

    홈페이지에 식당 카드를 띄웠습니다.

--- mealmate/src/Routes/Home.js
+++ mealmate/src/Routes/Home.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 function Home() {
   const [restaurantData, setRestaurantData] = useState(0); // 상태 초기화
@@ -9,7 +11,7 @@ function Home() {
       method: 'get',
       url: `https://api.meal-mate.shop/api/restaurant`,
       params: {
-        type: '분식'  // 쿼리 파라미터 key-value
+        type: ''  // 쿼리 파라미터 key-value
       }
     })
       .then((response) => {
@@ -25,7 +27,24 @@ function Home() {
     return (
       <div>
         {
-          console.log(restaurantData)
+          <Container>
+            <Row>
+              {
+                restaurantData.map((data) => {
+                  return (
+                    <Col>
+                      <Link to={'restaurant/' + data.restaurantId} style={{ textDecorationLine: 'none' }}>
+                        <img src={data.restaurantImageUrl} height={200} width={200} alt='식당이미지' />
+                        <h4 style={{ color: 'black' }}>{data.restaurantName}</h4>
+                        <p style={{ color: 'black' }}>{data.restaurantType}</p>
+                        <p style={{color: 'black'}}>{data.likeCount}</p>
+                      </Link>
+                    </Col>
+                  )
+                })
+              }
+            </Row>
+          </Container>
         }
       </div>
     )
