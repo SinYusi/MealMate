4fe2adc399a2e4aa807a6287a0c145b8d6c0f666
SinYusi <yusi4781@naver.com>
2024-05-29 00:28:11 +0900

    식당 디테일 페이지 구현 성공

--- mealmate/src/Routes/Restaurant.jsx
+++ mealmate/src/Routes/Restaurant.jsx
@@ -4,30 +4,31 @@ import { Col, Container, Row } from "react-bootstrap";
 import { useParams } from "react-router-dom"
 
 function Restaurant() {
-  const { id } = useParams()
+  const restaurantId = useParams().id
   let [restaurant, setRestaurant] = useState({})
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(`https://api.meal-mate.shop/api/restaurant/${id}`,{
-          params: {
-            restaurantId: parseInt(id)
-          }
-        })
-        setRestaurant(response)
+        const response = await axios.get(`https://api.meal-mate.shop/api/restaurant/${restaurantId}`)
+        setRestaurant(response.data)
       } catch (error) {
         console.log(error)
       }
     }
-  }, [id])
-  console.log(restaurant)
+    fetchData();
+  }, [])
+
   return (
     <Container>
       <Row>
         <Col>
-          <img src={restaurant.restaurantImageUrl} alt='식당사진' />
+          <h3>{restaurant.restaurantName}</h3>
+          <img src={restaurant.restaurantImageUrl} alt='식당사진' style={{ width: '100%' }} />
         </Col>
-        <Col>2 of 2</Col>
+        <Col>
+          <p style={{marginTop: '40px'}}>여는 시간 : {restaurant.openAt}</p>
+          <p>닫는 시간 : {restaurant.closeAt}</p>
+        </Col>
       </Row>
     </Container>
   )
