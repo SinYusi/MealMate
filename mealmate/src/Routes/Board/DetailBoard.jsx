623f09225fabdbcd30f4407d33897f00c2592fe3
SinYusi <yusi4781@naver.com>
2024-06-01 00:18:24 +0900

    댓글 기능 구현

--- mealmate/src/Routes/Board/DetailBoard.jsx
+++ mealmate/src/Routes/Board/DetailBoard.jsx
@@ -1,21 +1,33 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
+import { useCookies } from "react-cookie"
+import { useNavigate, useParams } from "react-router-dom"
 
 function DetailBoard() {
   const boardId = useParams().id
   let [board, setBoard] = useState({})
+  let [comment, setComment] = useState([])
+  let [isComment, setIsComment] = useState(false)
   useEffect(() => {
-    const fetchData = async () => {
+    const loadDetailData = async () => {
       try {
         const response = await axios.get(`https://api.meal-mate.shop/api/board/${boardId}`)
         setBoard(response.data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    loadDetailData();
+    const loadCommentData = async () => {
+      try {
+        const response = await axios.get(`https://api.meal-mate.shop/api/comment/${boardId}`)
+        setComment(response.data)
         console.log(response.data)
       } catch (error) {
         console.log(error)
       }
     }
-    fetchData();
+    loadCommentData()
   }, [boardId])
 
   return (
@@ -24,7 +36,62 @@ function DetailBoard() {
       <p>작성자 : {board.email}</p>
       <p>올린 시간 : {board.lastTime}</p>
       <p>{board.content}</p>
+      <fieldset>
+        <legend>댓글</legend>
+        {<Comment comment={comment} />}
+      </fieldset>
+      <button onClick={() => setIsComment(true)}>댓글쓰기</button>
+      {isComment === true ? <AddComment boardId={boardId} /> : null}
     </>
+  )
+}
+
+function Comment({ comment }) {
+  return (
+    <div>
+      {comment.map((data) => (
+        <div key={data.commentId} style={{ position: 'relative', marginBottom: '1em' }}>
+          <div>
+            <p>{data.commentContent}</p>
+            <p>{data.createDt}</p>
+          </div>
+          <div>
+            <p>{data.email}</p>
+            <p>{data.createDt}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+function AddComment(props) {
+  const navigate = useNavigate()
+  const [cookies] = useCookies(['access_token'])
+  const [addComment, setAddComment] = useState(null)
+  const [success, setSuccess] = useState(false)
+  const handleSubmit = async (e) => {
+    const token = cookies.access_token
+    try {
+      await axios.post(`https://api.meal-mate.shop/api/comment/${props.boardId}`, {
+        commentContent: addComment
+      }, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+      setSuccess(true)
+    } catch (error) {
+      console.log(error)
+    }
+    if (success === true)
+      navigate('./')
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" placeholder="댓글을 쓰세요" onChange={(e) => { setAddComment(e.target.value) }} />
+      <button type="submit">추가</button>
+    </form>
   )
 }
 
