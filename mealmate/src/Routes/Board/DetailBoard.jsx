d9058fdeae0f9eb7b388becddc998be7c08d94e5
SinYusi <yusi4781@naver.com>
2024-06-08 00:34:26 +0900

    게시글 삭제 기능 구현

--- mealmate/src/Routes/Board/DetailBoard.jsx
+++ mealmate/src/Routes/Board/DetailBoard.jsx
@@ -10,20 +10,20 @@ function DetailBoard() {
   let [comment, setComment] = useState([])
   let [isComment, setIsComment] = useState(false)
   const [cookies] = useCookies(['access_token'])
-  const [isMatched, setIsMatched] = useState(false)
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
-    const loadDetailData = async () => {
-      const token = cookies.access_token
+    const token = cookies.access_token
+    if (token) {
       const decodedToken = jwtDecode(token)
+      setUser(decodedToken)
+      console.log('토큰 디코드 성공')
+    }
+    const loadDetailData = async () => {
       try {
-        const response = await axios.get(`https://api.meal-mate.shop/api/board/${boardId}`, {
-          headers: {
-            Authorization: `Bearer ${token}`
-          }
-        })
+        const response = await axios.get(`https://api.meal-mate.shop/api/board/${boardId}`)
         setBoard(response.data)
-        if (decodedToken.sub == board.email) { setIsMatched(true) }
+        console.log('게시물 데이터 불러오기 성공')
       } catch (error) {
         console.log(error)
       }
@@ -33,19 +33,30 @@ function DetailBoard() {
       try {
         const response = await axios.get(`https://api.meal-mate.shop/api/comment/${boardId}`)
         setComment(response.data)
-        console.log(response.data)
+        console.log('댓글 데이터 불러오기 성공')
       } catch (error) {
         console.log(error)
       }
     }
     loadCommentData()
-  }, [boardId])
+  }, [boardId, cookies])
+
+  let isMatched = false
+
+  if (user) {
+    isMatched = board.email === user.sub
+    console.log('글 작성자와 일치')
+  }
+
   return (
     <>
-      <h2>{board.title}</h2>
-      <p>작성자 : {board.email}</p>
-      <p>올린 시간 : {board.lastTime}</p>
-      <p>{board.content}</p>
+      <div>
+        <h2>{board.title}</h2>
+        <p>작성자 : {board.email}</p>
+        <p>올린 시간 : {board.lastTime}</p>
+        <p>{board.content}</p>
+        <DeleteAndFetchBoard isMatched={isMatched} boardId={boardId} />
+      </div>
       <fieldset>
         <legend>댓글</legend>
         {<Comment comment={comment} />}
@@ -56,18 +67,41 @@ function DetailBoard() {
   )
 }
 
+function DeleteAndFetchBoard(props) {
+  const navigate = useNavigate();
+  const deleteBoard = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.delete(`https://api.meal-mate.shop/api/board/${props.boardId}`)
+      console.log('게시물 삭제 완료')
+      navigate('/board')
+    } catch (error) {
+      console.log('게시물 삭제 중 오류 발생')
+    }
+  }
+  return (
+    <>
+      {props.isMatched ?
+        <form onSubmit={deleteBoard}>
+          <button type="submit">글 삭제</button>
+        </form>
+        : null
+      }
+    </>
+  )
+}
+
 function Comment({ comment }) {
   return (
     <div>
       {comment.map((data) => (
         <div key={data.commentId} style={{ position: 'relative', marginBottom: '1em' }}>
-          <div>
+          <div style={{display: 'flex'}}>
             <p>{data.commentContent}</p>
             <p>{data.createDt}</p>
           </div>
           <div>
             <p>{data.email}</p>
-            <p>{data.createDt}</p>
           </div>
         </div>
       ))}
