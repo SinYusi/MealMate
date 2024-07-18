06bc959676834816aebad59e4f998ba831c58edf
Yusi <yusi4781@naver.com>
2024-07-18 23:47:23 +0900

    MyPage 제작

--- mealmate/src/Routes/User/MyPage.jsx
+++ mealmate/src/Routes/User/MyPage.jsx
@@ -1,7 +1,41 @@
+import { jwtDecode } from "jwt-decode";
+import { useEffect, useState } from "react"
+import { useCookies } from "react-cookie";
+import Table from 'react-bootstrap/Table';
+
 export default function MyPage() {
+  const [loading, setLoading] = useState(true);
+  const [cookies] = useCookies(['access_token'])
+  const [decodedToken, setDecodedToken] = useState(null)
+
+  useEffect(() => {
+    const token = cookies.access_token
+    if (token)
+      setDecodedToken(jwtDecode(token))
+
+    setLoading(false)
+  }, [cookies.access_token])
+
+  if (loading) {
+    return (
+      <div>loading</div>
+    )
+  }
+
+  console.log(decodedToken)
+
   return (
     <>
-
+      <div style={{ marginLeft: '20%', marginRight: '20%' }}>
+        <Table striped="columns">
+          <thead>
+            <tr>
+              <th>Email</th>
+              <th>{decodedToken.sub}</th>
+            </tr>
+          </thead>
+        </Table>
+      </div>
     </>
   )
 }
